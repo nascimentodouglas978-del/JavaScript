6cc507dd756bb9b1924c641af3043e8df52325cf
Douglas nunes <nascimentodouglas978@gmail.com>
2026-09-08 14:24:34 -0300

    add array.js

---
+++ tipos de dados Array.js
@@ -0,0 +1,5 @@
+let frutas = ["maçã", "banana", "laranja"]; // array de string
+let numeros = [1, 2, 3, 4, 5]; // Array de números
+let misto = [true, "texto", 42]; // array com diferentes tipos de dados
+
+console.log(frutas, numeros , misto)
