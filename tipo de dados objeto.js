77095b9a3bfafb4c80971895e5a6dbfdf96d33d9
Douglas nunes <nascimentodouglas978@gmail.com>
2026-09-08 14:35:43 -0300

    add objeto.js

---
+++ tipo de dados objeto.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "Carlos",
+    idade: 28,
+    casado: true,
+};
+// Acessando propriedades do objeto
+console.log(pessoa.nome); // "Carlos"
+console.log(pessoa.idade); // 28
